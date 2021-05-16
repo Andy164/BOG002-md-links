03b294f678ce90e937c53c27bbe97cc7eb509078
Andy164 <apenarandao@ufpso.edu.co>
2021-05-15 20:34:55 -0500

    Configure mdLinks function and MDLinkSystem class

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
+// eslint-disable-next-line import/extensions
+import MDLink from "./lib/LinkSystem.js";
+
+const mdLinks = (path, options) => {
+  const arg2 = typeof options === "undefined" ? { validate: false } : options;
+  const linkSystem = new MDLink(path, arg2);
+  return new Promise((resolve, reject) => {
+    linkSystem.init((err, info) => (err ? reject(err) : resolve(info)));
+  });
 };
+
+mdLinks("..\\README.md")
+  .then((r) => console.log(r)).catch((e) => console.log(e));
