e08e6ded0be8b4daca7c16bd4590c7ecd1c1f4b3
Andy164 <apenarandao@ufpso.edu.co>
2021-05-24 07:43:04 -0500

    Export function

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // eslint-disable-next-line import/extensions
 import MDLink from "./lib/LinkSystem.js";
 
-const mdLinks = (path, options) => {
-  const arg2 = typeof options === "undefined" ? { validate: false } : options;
-  const linkSystem = new MDLink(path, arg2);
+const mdLinks = (path, options = { validate: false }) => {
+  const arg2 =
+    typeof options !== "object"
+      ? `The "options" argument must be of type object. Received type ${typeof options} (${options}).`
+      : false;
+  const propArg2 = Object.keys(options).includes("validate")
+    ? false
+    : `The "options" argument must be an object with a "validate" property.`;
+  const linkSystem =
+    typeof path === "string" ? new MDLink(path, options) : false;
+  const error = arg2 || propArg2;
   return new Promise((resolve, reject) => {
-    linkSystem.init((err, info) => (err ? reject(err) : resolve(info)));
+    if (!linkSystem)
+      reject(
+        new Error(
+          `The "path" argument must be of type string. Received type ${typeof path} (${path}).`
+        )
+      );
+    else if (error) {
+      reject(new Error(error));
+    } else linkSystem.init((err, info) => (err ? reject(err) : resolve(info)));
   });
 };
 
-mdLinks("..\\README.md")
-  .then((r) => console.log(r)).catch((e) => console.log(e));
+export default mdLinks;
